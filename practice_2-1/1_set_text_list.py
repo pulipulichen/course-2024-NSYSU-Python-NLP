68e26d0e12e9762469f17a1df413d4505745b217
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-05 21:59:52 +0800

    西元2024年12月05日 (週四) 21時59分52秒 CST

--- practice_2-1/1_set_text_list.py
+++ practice_2-1/1_set_text_list.py
@@ -1,8 +1,8 @@
 """
-設定text_array
+設定text_list
 """
 
-# Initialize the text_array variable.
-text_array = [
+# Initialize the 設定text_list variable.
+text_list = [
   "彰化保安林生態護坡見效 3年監測PM2.5濃度減26%"
 ]
--- practice_2-1/3_execute_ner.py
+++ practice_2-1/3_execute_ner.py
@@ -2,11 +2,11 @@
 執行NER分析
 """
 
-# Perform NER on text_array and store the result in the ner variable.
-ner = ner_driver(text_array)
+# Perform NER on text_list and store the result in the ner variable.
+ner = ner_driver(text_list)
 
 # Show results.
-for sentence, sentence_ner in zip(text_array, ner):
+for sentence, sentence_ner in zip(text_list, ner):
    print(sentence)
    for entity in sentence_ner:
       print(entity)
