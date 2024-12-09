fecfa8d799b124eb49f0b00df556df713a6eaf77
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-12-10 07:47:41 +0800

    Update 3_execute_translation.py

--- practice_1-2/3_execute_translation.py
+++ practice_1-2/3_execute_translation.py
@@ -4,7 +4,7 @@
 
 # Translate input_string to language code
 # Language code list: https://cloud.google.com/translate/docs/languages
-output_string = GoogleTranslator(source='auto', target=dest_lang_code).translate(text=input_string)
+output_string = GoogleTranslator(source='auto', target=target_lang_code).translate(text=input_string)
 
 # Check the result of translation
 output_string
