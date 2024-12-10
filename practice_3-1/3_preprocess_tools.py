c2ef2dda656e19d5248839fdf11f46e8bffaa760
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-12-10 08:25:20 +0800

    Update 3_preprocess_tools.py

--- practice_3-1/3_preprocess_tools.py
+++ practice_3-1/3_preprocess_tools.py
@@ -2,33 +2,24 @@
 準備前處理工具
 """
 
-# Install Google Translator package `googletrans` version 3.1.0a0.
-# Package detail: https://pypi.org/project/googletrans/
-!pip install googletrans
+# Install `opencc` package.
+# Package detail: https://pypi.org/project/OpenCC/
+!pip install opencc==1.1.9
 
-# Import Google Translator package.
-from googletrans import Translator
-
-# Initialize the translator object.
-# translator = Translator(service_urls=['translate.google.com'])
-translator = Translator()
-
-# Defind the destination language.
-# Language code list: https://cloud.google.com/translate/docs/languages
-dest_lang_code = 'zh-tw'
+# Import OpenCC package for conversion.
+import opencc
 
 # Remove symbols and convert Traditional Chinese text to Simplified Chinese.
 def preprocess_text(text: str) -> str:
+    
     # Remove special symbols.
     text = re.sub(r"[^\u4e00-\u9fff\d.a-zA-Z%+\-。！？，、；：（）【】《》“”‘’]", '', text)
 
-    # Translate input_string to language code
-    # Language code list: https://cloud.google.com/translate/docs/languages
-    output_result = translator.translate(input_string, dest=dest_lang_code)
+    # Initialize OpenCC converter.
+    converter = opencc.OpenCC('t2s')  # 't2s' stands for Traditional to Simplified
 
-    # Get the text of the result of translation
-    result = output_result.text
+    # Convert the text to Simplified Chinese.  
+    result = converter.convert(text)
 
     # Returns a string.
     return result
-
